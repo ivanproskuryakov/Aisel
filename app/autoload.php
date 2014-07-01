47e6e6ba82d12f4050494c03df941f615a71fa5f
invoice <volgodark@gmail.com>
2014-07-01 12:49:05 +0400

    Code fixing with insight.sensiolabs.com

--- app/autoload.php
+++ app/autoload.php
@@ -8,6 +8,10 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+// PHP Fatal error:  Maximum function nesting level of '100' reached
+// Set xDebug nesting level to 1000
+ini_set('xdebug.max_nesting_level', 1000);
+
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
