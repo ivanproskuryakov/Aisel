8e05fd1d0bf596f96aab0aedc4ae595effc21a92
Ivan Proskuryakov <volgodark@gmail.com>
2014-10-03 15:40:39 +0200

    FE: EN locale

--- web/app/code/core/resource/directives/main.js
+++ web/app/code/core/resource/directives/main.js
@@ -8,7 +8,7 @@ angular.module('aiselApp')
             element.bind('keyup', function (e) {
                 if (e.keyCode === 13) {
                     if(attrs.ngSearchRedirect.length > 1){
-                        window.location.assign('#/en/search/'+attrs.ngSearchRedirect);
+                        window.location.assign('/#/en/search/'+attrs.ngSearchRedirect);
                     }
                 }
             });
