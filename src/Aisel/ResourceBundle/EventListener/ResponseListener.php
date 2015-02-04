d31be5ec4f9b4157db4e5efb106380c782067ac5
ivanproskuryakov <volgodark@gmail.com>
2015-02-04 04:25:36 +0300

    Separate Folders 6

--- src/Aisel/ResourceBundle/EventListener/ResponseListener.php
+++ src/Aisel/ResourceBundle/EventListener/ResponseListener.php
@@ -18,9 +18,11 @@ class ResponseListener
             return;
         }
 
+        if (strpos($request->getRequestUri(), '/api/') > -1) $allowOrigin = 'http://ecommerce.aisel.dev';
+        if (strpos($request->getRequestUri(), '/admin/api/') > -1) $allowOrigin = 'http://admin.ecommerce.aisel.dev';
+
         $event->getResponse()->headers->set('Access-Control-Allow-Credentials', 'true');
-        $event->getResponse()->headers->set('Access-Control-Allow-Origin', 'http://ecommerce.aisel.dev');
-//        $event->getResponse()->headers->set('Access-Control-Allow-Origin', 'http://admin.ecommerce.aisel.dev');
+        $event->getResponse()->headers->set('Access-Control-Allow-Origin', $allowOrigin);
         $event->getResponse()->headers->set('Access-Control-Allow-Headers', 'origin, x-requested-with, content-type');
         $event->getResponse()->headers->set('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     }
