409ff08b10044e86bfc9ea2ef2e3d57da8886fb2
ivanproskuryakov <volgodark@gmail.com>
2015-03-08 23:55:17 +0100

    login tests with protractor

--- backend/protractor/modules/user.js
+++ backend/protractor/modules/user.js
@@ -10,10 +10,36 @@
 
 describe("E2E: User module tests", function () {
     console.log('Test loaded: User');
+    var ptor = protractor.getInstance();
 
-    it('Sign in route is working', function () {
+    it('Sign in route is working and log-in with username and password', function () {
         browser.get('http://admin.aisel.dev/en/user/login/');
         var el = element(by.css('.page-header'));
+
         expect((el).isDisplayed()).toBeTruthy();
+    });
+
+    it('Log-in with wrong username and password', function () {
+        browser.get('http://admin.aisel.dev/en/user/login/');
+        element(by.model('username')).sendKeys('wrong-backenduser');
+        element(by.model('password')).sendKeys('backenduser');
+        element(by.id('login-button')).click().then(function () {
+            ptor.getCurrentUrl().then(function (url) {
+
+                expect(url).toBe('http://admin.aisel.dev/en/user/login/');
+            });
+        });
+    });
+
+    it('Log-in with correct username and password', function () {
+        browser.get('http://admin.aisel.dev/en/user/login/');
+        element(by.model('username')).sendKeys('backenduser');
+        element(by.model('password')).sendKeys('backenduser');
+        element(by.id('login-button')).click().then(function () {
+            ptor.getCurrentUrl().then(function (url) {
+
+                expect(url).toBe('http://admin.aisel.dev/en/');
+            });
+        });
     });
 });
