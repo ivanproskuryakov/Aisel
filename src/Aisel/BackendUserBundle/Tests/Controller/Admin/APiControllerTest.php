816b23b4a108951860015fed990c29dfba45c301
ivanproskuryakov <volgodark@gmail.com>
2015-05-01 16:34:34 +0300

    Updating backend

---
+++ src/Aisel/BackendUserBundle/Tests/Controller/Admin/APiControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the Aisel package.
+ *
+ * (c) Ivan Proskuryakov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Aisel\BackendUserBundle\Tests\Controller\Admin;
+
+use Aisel\ResourceBundle\Tests\AbstractWebTestCase;
+
+/**
+ * ApiControllerTest
+ *
+ * @author Ivan Proskoryakov <volgodark@gmail.com>
+ */
+class ApiControllerTest extends AbstractWebTestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testGetUsersAction()
+    {
+        $this->client->request(
+            'GET',
+            '/backend/api/backenduser/',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json']
+        );
+
+        $response = $this->client->getResponse();
+        $content = $response->getContent();
+        $statusCode = $response->getStatusCode();
+
+        $this->assertTrue(200 === $statusCode);
+        $this->assertJson($content);
+    }
+
+    public function testGetUserAction()
+    {
+        $city = $this
+            ->em
+            ->getRepository('Aisel\BackendUserBundle\Entity\BackendUser')
+            ->findOneBy(['username' => 'backenduser']);
+
+        $this->client->request(
+            'GET',
+            '/backend/api/backenduser/' . $city->getId(),
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json']
+        );
+
+        $response = $this->client->getResponse();
+        $content = $response->getContent();
+        $statusCode = $response->getStatusCode();
+        $result = json_decode($content, true);
+
+        $this->assertTrue(200 === $statusCode);
+        $this->assertEquals($result['id'], $city->getId());
+    }
+
+}
