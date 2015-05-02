f8c22ae173453af523e1ae506e5ecf73da503869
ivanproskuryakov <volgodark@gmail.com>
2015-05-02 22:25:36 +0300

    Adding tests for backend

---
+++ src/Aisel/ConfigBundle/Tests/Controller/ApiControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Aisel package.
+ *
+ * (c) Ivan Proskuryakov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Aisel\ConfigBundle\Tests\Controller;
+
+use Aisel\ResourceBundle\Tests\AbstractWebTestCase;
+
+/**
+ * ApiControllerTest
+ *
+ * @author Ivan Proskoryakov <volgodark@gmail.com>
+ */
+class ApiControllerTest extends AbstractWebTestCase
+{
+
+    /**
+     * @var array
+     */
+    private $config = array();
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->config = static::$kernel->getContainer()->getParameter('aisel_config');
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testConfigAction()
+    {
+
+        $this->client->request(
+            'GET',
+            '/api/en/config/'
+        );
+
+        $response = $this->client->getResponse();
+        $content = $response->getContent();
+        $statusCode = $response->getStatusCode();
+        $result = json_decode($content, true);
+
+        $this->assertJson($content);
+        $this->assertTrue(200 === $statusCode);
+        $this->assertTrue(isset($result['settings']));
+        $this->assertTrue(isset($result['locale']));
+
+        foreach ($result['locale']['available'] as $locale) {
+            $this->assertNotFalse(array_search($locale, $this->locales));
+        }
+    }
+
+}
