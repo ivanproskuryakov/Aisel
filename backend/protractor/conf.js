ae8f69c0deedc7b596705cc39543c10c9da01ef0
ivanproskuryakov <volgodark@gmail.com>
2015-05-04 18:42:49 +0300

    Travis CI

--- backend/protractor/conf.js
+++ backend/protractor/conf.js
@@ -23,5 +23,15 @@ exports.config = {
         'modules/orders.js',
         'modules/products.js',
         'modules/page.js',
-    ]
-}
+    ],
+
+    // ----- Capabilities to be passed to the webdriver instance.
+    // For a full list of available capabilities, see
+    // https://code.google.com/p/selenium/wiki/DesiredCapabilities
+    // and
+    // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
+    capabilities: {
+        'browserName': 'chrome'
+    }
+
+};
