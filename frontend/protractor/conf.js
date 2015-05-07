4c84430b6ca4db3875a8895f5c90fe6eb309b631
ivanproskuryakov <volgodark@gmail.com>
2015-05-08 00:31:25 +0300

    Travis CI

--- frontend/protractor/conf.js
+++ frontend/protractor/conf.js
@@ -14,5 +14,18 @@
 
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['modules/*.js']
-}
+    specs: [
+        'modules/*.js'
+    ],
+
+    // ----- Capabilities to be passed to the webdriver instance.
+    // For a full list of available capabilities, see
+    // https://code.google.com/p/selenium/wiki/DesiredCapabilities
+    // and
+    // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
+    capabilities: {
+        'browserName': 'firefox'
+        //'browserName': 'chrome'
+        //'chromeOptions': { args: ['--test-type', 'show-fps-counter=true'] }
+    }
+};
