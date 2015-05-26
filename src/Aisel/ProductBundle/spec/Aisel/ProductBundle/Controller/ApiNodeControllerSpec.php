6785b91b7e2eef11245dfcff23bfb010ff6e8e33
ivanproskuryakov <volgodark@gmail.com>
2015-05-26 14:01:11 +0300

    phpspec

--- src/Aisel/ProductBundle/spec/Aisel/ProductBundle/Controller/ApiNodeControllerSpec.php
+++ src/Aisel/ProductBundle/spec/Aisel/ProductBundle/Controller/ApiNodeControllerSpec.php
@@ -20,7 +20,7 @@ class ApiCategoryControllerSpec extends ObjectBehavior
 {
     public function it_is_initializable()
     {
-        $this->shouldHaveType('\Aisel\ProductBundle\Controller\ApiCategoryController');
+        $this->shouldHaveType('\Aisel\ProductBundle\Controller\ApiNodeController');
     }
 
 }
