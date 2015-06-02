de4e44e7384baf4967d601221e343eed16684eaa
ivanproskuryakov <volgodark@gmail.com>
2015-06-03 00:41:19 +0300

    bower allow root

--- bower.sh
+++ bower.sh
@@ -3,7 +3,7 @@ rm -rf ./backend/web/bower_components/
 rm -rf ./backend/web/bower_components/
 
 cd ./frontend/
-bower install -f
+bower install -f --allow-root
 cd ./../
 cd ./backend/
-bower install -f
+bower install -f --allow-root
