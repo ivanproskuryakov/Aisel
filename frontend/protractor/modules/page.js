a41c6bfb283f773ad11a437293916d5f185426e2
ivanproskuryakov <volgodark@gmail.com>
2015-06-24 14:17:07 +0300

    Adding frontend tests

--- frontend/protractor/modules/page.js
+++ frontend/protractor/modules/page.js
@@ -21,6 +21,17 @@ describe("E2E: Page module tests", function () {
         expect(browser.getTitle()).toEqual('Aisel - open source project');
     });
 
+    it('View category route is working', function () {
+        browser.get('http://aisel.dev/en/page/categories/');
+
+        element(by.css('.pageList h2 a')).click().then(function () {
+            browser.getCurrentUrl().then(function (url) {
+                expect(url.indexOf("/page/category/")).toBeGreaterThan(0);
+                expect( element(By.css('.page-header h2')).getText()).not.toBeNull();
+            });
+        });
+    });
+
     it('About Us page route is working', function () {
         browser.get('http://aisel.dev/en/page/view/en-about-aisel/');
         expect(browser.getTitle()).toEqual('About Us');
