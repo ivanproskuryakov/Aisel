a4349efd80ed4e91a084dfc41641c3d20d37ef7a
Ivan Proskuryakov <volgodark@gmail.com>
2015-07-12 14:47:00 +0300

    Media bundle

--- src/Aisel/MediaBundle/DependencyInjection/AiselMediaExtension.php
+++ src/Aisel/MediaBundle/DependencyInjection/AiselMediaExtension.php
@@ -21,7 +21,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class AiselProductExtension extends Extension
+class AiselMediaExtension extends Extension
 {
     /**
      * {@inheritDoc}
