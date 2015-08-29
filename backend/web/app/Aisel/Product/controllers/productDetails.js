d4a56437b200594f498bca172dab926f5bde0c67
Ivan Proskuryakov <volgodark@gmail.com>
2015-08-29 16:49:39 +0200

    Product images

--- backend/web/app/Aisel/Product/controllers/productDetails.js
+++ backend/web/app/Aisel/Product/controllers/productDetails.js
@@ -12,75 +12,72 @@
  * @description     ProductDetailsCtrl
  */
 
-define(['app'], function(app) {
+define(['app'], function (app) {
     app.controller('ProductDetailsCtrl',
-        function($controller, $stateParams, $scope, resourceService, Environment, mediaService, notify) {
+        function ($controller, $stateParams, $scope, resourceService, Environment, mediaService, notify) {
 
-        $scope.route = {
-            name: 'Product',
-            collection: 'products',
-            edit: 'productEdit'
-        };
-
-        var itemService = new resourceService('product');
-
-        angular.extend(this, $controller('AbstractDetailsCategoryCtrl', {
-            $scope: $scope,
-            itemService: itemService
-        }));
-
-        // Product Images
-        $scope.domain = Environment.settings.domain;
-        $scope.uploadPath = Environment.settings.api + '/media/image/upload/?id=' + $stateParams.id;
-
-        $scope.fileDelete = function (id) {
-            mediaService.delete(id).success(
-                function(data, status) {
-                    notify('Item removed');
-                    angular.forEach($scope.item.images, function(image, key) {
-                        if (image.id === id) {
-                            $scope.item.images.splice(
-                                $scope.item.images.indexOf(image),1
-                            );
-                        }
-                    });
-                }
-            ).error(
-                function(data, status) {
-                    if (data.error.code == 404) {
-                        $state.transitionTo('home', {
-                            locale: locale
-                        });
-                        notify('404 Noting found');
-                    } else {
-                        notify(data.error.message);
-                    }
-                }
-            )
-        };
-
-        $scope.fileUploaded = function ($file, $message, $flow) {
-            var uploadedImage = JSON.parse($message);
-            var image = {
-                filename: uploadedImage,
-                title: '',
-                description: '',
-                product: {
-                    id: $stateParams.id
-                }
+            $scope.route = {
+                name: 'Product',
+                collection: 'products',
+                edit: 'productEdit'
             };
 
-            mediaService.new(image).success(
-                function(data, status, headers, config) {
-                    var fileId = headers("Location").split('/').pop();
-                    image.id = fileId;
-                    image.created_at = new Date();
-                    image.updated_at = new Date();
+            var itemService = new resourceService('product');
 
-                    $scope.item.images.push(image);
-                }
-            );
-        };
+            angular.extend(this, $controller('AbstractDetailsCategoryCtrl', {
+                $scope: $scope,
+                itemService: itemService
+            }));
 
-    });
+            // Product Images
+            $scope.domain = Environment.settings.domain;
+            $scope.uploadPath = Environment.settings.api + '/media/image/upload/?id=' + $stateParams.id;
+
+            $scope.fileDelete = function (id) {
+                mediaService.delete(id).success(
+                    function (data, status) {
+                        notify('Item removed');
+                        angular.forEach($scope.item.images, function (image, key) {
+                            if (image.id === id) {
+                                $scope.item.images.splice(
+                                    $scope.item.images.indexOf(image), 1
+                                );
+                            }
+                        });
+                    }
+                ).error(
+                    function (data, status) {
+                        if (data.error.code == 404) {
+                            $state.transitionTo('home', {
+                                locale: locale
+                            });
+                            notify('404 Noting found');
+                        } else {
+                            notify(data.error.message);
+                        }
+                    }
+                )
+            };
+
+            $scope.fileUploaded = function ($file, $message, $flow) {
+                var uploadedImage = JSON.parse($message);
+                var image = {
+                    filename: uploadedImage,
+                    title: '',
+                    description: '',
+                    product: {
+                        id: $stateParams.id
+                    }
+                };
+
+                mediaService.new(image).success(
+                    function (data, status, headers, config) {
+                        image.id = headers("Location").split('/').pop();
+                        $scope.item.images.push(image);
+                        $scope.editSave();
+                    }
+                );
+            };
+
+        });
 });
