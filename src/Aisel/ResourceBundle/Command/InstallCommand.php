7f46ea2d191b2ebb1dcaaae36d61592e6351eaa2
Ivan Proskuryakov <volgodark@gmail.com>
2015-09-04 20:48:23 +0200

    installer fixes

--- src/Aisel/ResourceBundle/Command/InstallCommand.php
+++ src/Aisel/ResourceBundle/Command/InstallCommand.php
@@ -83,15 +83,9 @@ EOT
      */
     protected function setupDatabase(InputInterface $input, OutputInterface $output)
     {
-        $drop = $this->getApplication()->find('doctrine:database:drop');
-        $drop_args = array(
-            'command' => 'doctrine:database:drop',
-            '--force' => true
-        );
-        $drop->run(new ArrayInput($drop_args), $output);
-        $this->runCommand('doctrine:database:create', $input, $output);
-        $this->runCommand('doctrine:schema:create', $input, $output);
-        $this->runCommand('doctrine:fixtures:load', $input, $output);
+        $this->runCommand('doctrine:mongodb:schema:drop', $input, $output);
+        $this->runCommand('doctrine:mongodb:schema:create', $input, $output);
+        $this->runCommand('doctrine:mongodb:fixtures:load', $input, $output);
     }
 
     /**
@@ -177,7 +171,7 @@ EOT
     {
         $installedCommand = "command -v $command";
 
-        return (bool) shell_exec($installedCommand);
+        return (bool)shell_exec($installedCommand);
     }
 
 }
