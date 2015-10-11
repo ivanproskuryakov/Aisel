ef72806b088a6a89de8727b7ba6f4b11351adc0a
Ivan Proskuryakov <volgodark@gmail.com>
2015-10-11 04:51:55 +0200

    no message

--- frontend/web/app/Aisel/Product/controllers/productCategoryDetails.js
+++ frontend/web/app/Aisel/Product/controllers/productCategoryDetails.js
@@ -69,6 +69,7 @@ define(['app'], function(app) {
                     'id',
                     'ASC'
                 );
+                $scope.paginationPage = paginationPage;
             };
 
 
