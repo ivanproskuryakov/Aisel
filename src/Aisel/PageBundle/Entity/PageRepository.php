c9dc40f6e6c276db7b0cfd4f98aa56c985bd06e0
Ivan Proskuryakov <volgodark@gmail.com>
2015-11-24 00:23:23 +0300

    WIP - entities

--- src/Aisel/PageBundle/Entity/PageRepository.php
+++ src/Aisel/PageBundle/Entity/PageRepository.php
@@ -21,66 +21,66 @@ use Aisel\PageBundle\Entity\Page;
  */
 class PageRepository extends CollectionRepository
 {
-
-    /**
-     * Get pages based on limit, current pagination and search query
-     *
-     * @param  array $params
-     * @return Page
-     */
-    public function searchFromRequest($params)
-    {
-        $this->mapRequest($params);
-
-        $query = $this
-            ->getEntityManager()
-            ->createQueryBuilder($this->getDocumentName())
-            ->field('status')->equals(true)
-            ->field('locale')->equals($this->locale)
-            ->field('content')->equals(new \MongoRegex('/.*' . $this->search . '.*/i'));
-
-        $collection = $query
-            ->limit($this->pageLimit)
-            ->skip($this->pageSkip)
-            ->sort($this->order, $this->orderBy)
-            ->getQuery()
-            ->toArray();
-
-        return $collection;
-    }
-
-    /**
-     * Get page total
-     *
-     * @param array $params
-     *
-     * @return int $total
-     */
-    public function getTotalFromRequest(array $params)
-    {
-        $this->mapRequest($params);
-        $query = $this
-            ->getEntityManager()
-            ->createQueryBuilder($this->model);
-
-        if ($this->locale) {
-            $query->field('locale')->equals($this->locale);
-        }
-
-        if ($this->search != '') {
-            $query->field('content')->equals(new \MongoRegex('/.*' . $this->search . '.*/i'));
-        }
-
-        $query->field('status')->equals(true);
-        $total = $query->count()
-            ->getQuery()
-            ->execute();
-
-        if (!$total) {
-            return 0;
-        }
-
-        return $total;
-    }
+//
+//    /**
+//     * Get pages based on limit, current pagination and search query
+//     *
+//     * @param  array $params
+//     * @return Page
+//     */
+//    public function searchFromRequest($params)
+//    {
+//        $this->mapRequest($params);
+//
+//        $query = $this
+//            ->getEntityManager()
+//            ->createQueryBuilder($this->getDocumentName())
+//            ->field('status')->equals(true)
+//            ->field('locale')->equals($this->locale)
+//            ->field('content')->equals(new \MongoRegex('/.*' . $this->search . '.*/i'));
+//
+//        $collection = $query
+//            ->limit($this->pageLimit)
+//            ->skip($this->pageSkip)
+//            ->sort($this->order, $this->orderBy)
+//            ->getQuery()
+//            ->toArray();
+//
+//        return $collection;
+//    }
+//
+//    /**
+//     * Get page total
+//     *
+//     * @param array $params
+//     *
+//     * @return int $total
+//     */
+//    public function getTotalFromRequest(array $params)
+//    {
+//        $this->mapRequest($params);
+//        $query = $this
+//            ->getEntityManager()
+//            ->createQueryBuilder($this->model);
+//
+//        if ($this->locale) {
+//            $query->field('locale')->equals($this->locale);
+//        }
+//
+//        if ($this->search != '') {
+//            $query->field('content')->equals(new \MongoRegex('/.*' . $this->search . '.*/i'));
+//        }
+//
+//        $query->field('status')->equals(true);
+//        $total = $query->count()
+//            ->getQuery()
+//            ->execute();
+//
+//        if (!$total) {
+//            return 0;
+//        }
+//
+//        return $total;
+//    }
 
 }
